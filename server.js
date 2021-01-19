5389d70b83dec24a0707ad2826017c46e96dde57
Nikita <65064358+lesnik34@users.noreply.github.com>
2021-01-19 18:09:54 +0300

    fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.json());
 
 const chatStorage = {
